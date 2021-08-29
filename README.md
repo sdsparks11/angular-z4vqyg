afd0b9a9a1b07c5dd0f8a9a9b6e96c1fc4d88ecf
sdsparks11 <suberthi1998@gmail.com>
2021-08-29 08:49:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-z4vqyg-rww2qy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-z4vqyg-rww2qy)
---
+++ src/app/productdummy.ts
@@ -0,0 +1,18 @@
+// JS array with objects...
+export interface productdummy{
+  productId:number;
+  productName:string;
+  price:number;
+}
+export const productdummy = [
+  {
+    'productId':1,
+    'productName':'A tale of two cities',
+    'price':200
+  },
+  {
+    'productId':2,
+    'productName':'Oliver Twist',
+    'price':300
+  }
+];
